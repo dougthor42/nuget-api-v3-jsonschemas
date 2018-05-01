7537864b1f80e6831e605719ad914860fa275175
Douglas Thor <doug.thor@gmail.com>
2018-05-01 14:43:24 -0700

    Renamed vars in test_json.py to be a bit more descriptive.

--- test_json.py
+++ test_json.py
@@ -14,7 +14,7 @@ from jsonschema.exceptions import ValidationError
 METASCHEMA_FILE = Path("metaschema.json")
 # JsonSchema 2.6.0 only supports draft-04 at the time of this writing
 METASCHEMA_URL = "http://json-schema.org/draft-04/schema"
-FILES = list(f for f in Path('./schemas').iterdir() if f.suffix == ".json")
+SCHEMAS = list(f for f in Path('./schemas').iterdir() if f.suffix == ".json")
 
 
 @pytest.fixture
@@ -25,40 +25,40 @@ def metaschema():
     return data
 
 
-@pytest.mark.parametrize("file", FILES)
-def test_valid_json(file):
-    with open(str(file), 'r') as openf:
+@pytest.mark.parametrize("schema", SCHEMAS)
+def test_valid_json(schema):
+    with open(str(schema), 'r') as openf:
         try:
             json.load(openf)
         except Exception:
             pytest.fail("Invalid JSON file.")
 
 
-@pytest.mark.parametrize("file", FILES)
-def test_valid_jsonschema(metaschema, file):
-    with open(str(file), 'r') as openf:
+@pytest.mark.parametrize("schema", SCHEMAS)
+def test_valid_jsonschema(metaschema, schema):
+    with open(str(schema), 'r') as openf:
         data = json.load(openf)
         try:
             validate(data, metaschema)
         except ValidationError as err:
             msg = "Invalid JsonSchema file `{}`. Message:\n{}"
-            pytest.fail(msg.format(file, str(err)))
+            pytest.fail(msg.format(schema, str(err)))
         except Exception as err:
             msg = ("Unknown exception when validating JsonSchema file {}\n"
                    "  Message: {}")
-            pytest.fail(msg.format(file, str(err)))
+            pytest.fail(msg.format(schema, str(err)))
 
 
-@pytest.mark.parametrize("file", FILES)
-def test_sample_file(file):
+@pytest.mark.parametrize("schema", SCHEMAS)
+def test_sample_file(schema):
     # Make sure we have a sample file.
-    name = Path(".".join(file.name.split(".")[3:]))
-    sample_file = file.parent.parent / Path("samples") / name
+    name = Path(".".join(schema.name.split(".")[3:]))
+    sample_file = schema.parent.parent / Path("samples") / name
     if not sample_file.exists():
         msg = "Sample file {} not found."
         pytest.fail(msg.format(str(sample_file)))
 
-    with open(str(file), 'r') as schema_file:
+    with open(str(schema), 'r') as schema_file:
         schema_data = json.load(schema_file)
         with open(str(sample_file), 'r') as openf:
             data = json.load(openf)
@@ -68,10 +68,10 @@ def test_sample_file(file):
                 msg = ("Sample file validation failed.\n"
                        "  Schema: {}\n"
                        "  Sample: {}")
-                pytest.fail(msg.format(file, sample_file))
+                pytest.fail(msg.format(schema, sample_file))
             except Exception as err:
                 msg = ("Unknown exception occured when validating."
                        "  Schema: {}\n"
                        "  Sample: {}\n"
                        "  Message: {}")
-                pytest.fail(msg.format(file, sample_file, str(err)))
+                pytest.fail(msg.format(schema, sample_file, str(err)))
